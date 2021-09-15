bac44bd585a2780677bb32d16d25cfc206128782
たなかともあ <batman.vs.battamon@gmail.com>
2021-09-16 08:52:19 +0900

    edit init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -114,8 +114,10 @@
 (put 'downcase-region 'disabled nil)
 
 
-(when (string-equal system-type "darwin")
-  (set-language-environment 'Japanese))
+(set-language-environment 'Japanese)
+(set-keyboard-coding-system 'utf-8)
+(set-clipboard-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
 
 
 (require 'package)
